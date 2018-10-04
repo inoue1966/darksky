aa057ae68df6dcd50949f78d3a2290d41c8fc6b3
Masayuki INOUE <masayuki.inoue@jp.ricoh.com>
2018-10-04 16:31:40 +0900

    いくつか更新

--- ds.py
+++ ds.py
@@ -1,31 +1,46 @@
-from flask import Flask, render_template, make_response
 #import simplejson as json
 import json
 import requests
+from flask import Flask, render_template, make_response
 from darksky_secret import DARKSKYKEY
 
 
-app = Flask(__name__)
+class CustomFlask(Flask):
+    """ jinjaデリミタの変更 """
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        block_start_string='(%',
+        block_end_string='%)',
+        variable_start_string='((',
+        variable_end_string='))',
+        comment_start_string='(#',
+        comment_end_string='#)',
+    ))
+
+
+app = CustomFlask(__name__)
+
 
 @app.route('/index')
 def index():
     """index"""
-    return render_template('ds2.html', title='flask test')
+    return render_template('ds3.html', title='flask test')
 
 
 # APIの実装
 @app.route('/api', methods=['GET', 'POST'])
 def get():
-    url = "https://api.darksky.net/forecast/"+DARKSKYKEY+"/35.2637,139.1502?units=si&lang=ja&exclude=currently,minutely,alerts,flags"
+    """ get """
+    url = "https://api.darksky.net/forecast/"+DARKSKYKEY + \
+        "/35.2637,139.1502?units=si&lang=ja&exclude=currently,minutely,alerts,flags"
     ret = requests.get(url)
     jsondata = None
     if ret.status_code == 200:
         jsondata = ret.json()
-        
-        #print(jsondata)
-    return make_response(json.dumps(jsondata,  ensure_ascii=False))
+        # print(jsondata)
+    return make_response(json.dumps(jsondata, ensure_ascii=False))
 
 
-## おまじない
+# おまじない
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
